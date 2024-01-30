9f37cfab622dda33dd7966d100cf2c6f52dd8e13
luminousol <luminousolding@gmail.com>
2024-01-30 12:03:11 +0900

    ✨feat: PGS5. 자연수 뒤집어 배열로 만들기

--- src/soli/PGS/ch2_string/PGS5.java
+++ src/soli/PGS/ch2_string/PGS5.java
@@ -1,4 +1,20 @@
 package soli.PGS.ch2_string;
 
 public class PGS5 {
+    public int[] solution(long n) {
+
+        String str = Long.toString(n);
+        int[] answer = new int[str.length()];
+
+        int idx = 0;
+        int i = str.length() -1;
+
+        while(idx < str.length() && idx >= 0) {
+            answer[idx] = str.charAt(i) - '0';
+            idx++;
+            i--;
+        }
+
+        return answer;
+    }
 }
