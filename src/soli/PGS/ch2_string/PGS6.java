3efa00b6e07365120c16ca4fb301cee038415ca7
luminousol <luminousolding@gmail.com>
2024-02-01 00:06:30 +0900

    ✨feat: PGS6. 시저암호 - 테스트케이스 추가

--- src/soli/PGS/ch2_string/PGS6.java
+++ src/soli/PGS/ch2_string/PGS6.java
@@ -24,5 +24,8 @@ public class PGS6 {
     public static void main(String[] args) {
         PGS6 pgs6 = new PGS6();
 
+        System.out.println("Test case 1: " + pgs6.solution("AB", 1)); // "BC"
+        System.out.println("Test case 2: " + pgs6.solution("z", 1)); // "a"
+        System.out.println("Test case 3: " + pgs6.solution("a B z", 4)); // "e F d"
     }
 }
