c2b281670bb7a1f9e0d7f1120928faef7d2c303b
luminousol <luminousolding@gmail.com>
2024-02-04 20:50:20 +0900

    ✨feat: PGS10.이진 변환 반복하기

---
+++ src/soli/PGS/ch2_string/PGS10.java
@@ -0,0 +1,16 @@
+package soli.PGS.ch2_string;
+
+public class PGS10 {
+
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PGS10 pgs10 = new PGS10();
+        System.out.println("Test Case1 : " + pgs10.solution("110010101001"	));
+        System.out.println("Test Case2 : " + pgs10.solution("01110"));
+        System.out.println("Test Case3 : " + pgs10.solution("1111111"	));
+    }
+}
