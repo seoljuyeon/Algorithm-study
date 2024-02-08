8fa8c5165456efed49858f090a78177d1df7fa87
luminousol <luminousolding@gmail.com>
2024-02-08 11:34:26 +0900

    ✨feat: PGS11.숫자 문자열과 영단어

---
+++ src/soli/PGS/ch2_string/PGS12.java
@@ -0,0 +1,18 @@
+package soli.PGS.ch2_string;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/81301
+public class PGS12 {
+    public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PGS12 pgs12 = new PGS12();
+
+        System.out.println("Test Case 1 : " + pgs12.solution("one4seveneight"));
+        System.out.println("Test Case 2 : " + pgs12.solution("23four5six7"));
+        System.out.println("Test Case 3 : " + pgs12.solution("2three45sixseven"));
+        System.out.println("Test Case 2 : " + pgs12.solution("123"));
+    }
+}
