0b6f797d002ebfdae849e4e4b3647252e61c129c
luminousol <luminousolding@gmail.com>
2024-02-08 11:30:17 +0900

    ✨feat: PGS11.문자열 내 p와 y의 개수

---
+++ src/soli/PGS/ch2_string/PGS11.java
@@ -0,0 +1,19 @@
+package soli.PGS.ch2_string;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12916
+public class PGS11 {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        PGS11 pgs11 = new PGS11();
+
+        System.out.println("Test Case1 " + pgs11.solution("pPoooyY"));
+        System.out.println("Test Case1 " + pgs11.solution("Pyy"));
+
+    }
+}
