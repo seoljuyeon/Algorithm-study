746fda58346fcc20c98776a5e9b444352b347340
luminousol <luminousolding@gmail.com>
2024-02-11 11:35:42 +0900

    ✨feat: PGS11.숫자 문자열과 영단어

--- src/soli/PGS/ch2_string/PGS11.java
+++ src/soli/PGS/ch2_string/PGS11.java
@@ -1,11 +1,24 @@
 package soli.PGS.ch2_string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/12916
 public class PGS11 {
     boolean solution(String s) {
-        boolean answer = true;
 
-        return answer;
+        s = s.toLowerCase();
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < s.length() ; i++ ) {
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'y') {
+                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            }
+        }
+        if(map.get('p') ==  map.get('y')) {
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
