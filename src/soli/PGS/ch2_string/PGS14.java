2d29a9a946937f89706fb308dc7289e7288e037b
luminousol <luminousolding@gmail.com>
2024-02-13 14:21:21 +0900

    ✨feat: PGS14. 신규아이디 추천

---
+++ src/soli/PGS/ch2_string/PGS14.java
@@ -0,0 +1,20 @@
+package soli.PGS.ch2_string;
+
+public class PGS14 {
+    public String solution(String new_id) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PGS14 pgs14 = new PGS14();
+
+        System.out.println("Test Case1 : " + pgs14.solution("...!@BaT#*..y.abcdefghijklm"));
+        System.out.println("Test Case2 : " + pgs14.solution("z-+.^."));
+        System.out.println("Test Case3 : " + pgs14.solution("=.="));
+        System.out.println("Test Case4 : " + pgs14.solution("123_.def"));
+        System.out.println("Test Case5 : " + pgs14.solution("abcdefghijklmn.p"));
+
+    }
+
+}
