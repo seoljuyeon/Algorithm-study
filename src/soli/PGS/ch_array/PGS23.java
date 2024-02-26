b1d682caa0dd7f6f965cb7e6b2c4de1459f371fd
luminousol <luminousolding@gmail.com>
2024-02-26 15:59:19 +0900

    ✨feat: PGS23. k번째 수 찾기

--- src/soli/PGS/ch_array/PGS23.java
+++ src/soli/PGS/ch_array/PGS23.java
@@ -1,4 +1,34 @@
 package soli.PGS.ch_array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class PGS23 {
+
+    public static int[] solution(int[] array, int[][] commands) {
+
+        int[] answer = new int[commands.length];
+
+        for(int i = 0 ; i < commands.length ; i++) {
+
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            int a = commands[i][0];
+            int b = commands[i][1];
+            int c = commands[i][2];
+            for(int j = a-1 ; j < b ; j++) {
+                arr.add(array[j]);
+            }
+            Collections.sort(arr);
+            answer[i] = arr.get(c-1);
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Test case : " + Arrays.toString(solution(new int[]{1, 5, 2, 6, 5, 7, 4}, new int[][]{{2,5,3}, {4,4,1}, {1,7,3}})));
+    }
 }
