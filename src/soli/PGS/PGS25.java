d78763dd26358d56a1065d0b2e8e1450be178805
밝게빛나는 <130022922+luminousol@users.noreply.github.com>
2024-03-03 12:28:40 +0900

    ✨feat: PGS25. H-Index

---
+++ src/soli/PGS/PGS25.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class PGS25 {
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations);
+        int answer = 0;
+        int n = citations.length;
+        
+        for(int i = n ; i >= 0 ; i--) {
+            int count = 0;
+            for(int j = 0 ; j < n ; j++){
+                if(i <= citations[j]){
+                    count++;
+                }
+            }
+            if(count >= i) {
+                answer = i;
+                return answer;
+            }
+        }
+        
+        return answer;
+    }
+
+  public static void main(String[] args){
+    PGS25 pgs25 = new PGS25();
+    
+    System.out.println("Test case 1 :" + pgs25.solution(new int[]{3, 0, 6, 1, 5}));
+  }
+}
