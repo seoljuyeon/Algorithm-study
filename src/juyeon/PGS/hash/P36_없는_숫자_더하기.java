e8613f39efd46645871aeb5ca6c7d3b293bfa6c9
seoljuyeon <sxxljxxxxn@gmail.com>
2024-03-18 12:26:13 +0900

    ✨feat: PGS P36_없는_숫자_더하기 해시

--- src/juyeon/PGS/hash/P36_없는_숫자_더하기.java
+++ src/juyeon/PGS/hash/P36_없는_숫자_더하기.java
@@ -1,5 +1,7 @@
 package juyeon.PGS.hash;
 
+import java.util.HashSet;
+
 public class P36_없는_숫자_더하기 {
     public static void main(String[] args) {
         System.out.println(solution(new int[]{1, 2, 3, 4, 6, 7, 8, 0})); // 14
@@ -7,17 +9,31 @@ public class P36_없는_숫자_더하기 {
     }
 
     private static int solution(int[] numbers) {
-        int m = 0;
-        int n = 0;
+//        int m = 0;
+//        int n = 0;
+//
+//        for (int i = 0; i <= 9; i++) {
+//            m += i;
+//        }
+//
+//        for (int num : numbers) {
+//            n += num;
+//        }
+//
+//        return m - n;
+
+        // 해시
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : numbers) set.add(num);
+
+        int sum = 0;
 
         for (int i = 0; i <= 9; i++) {
-            m += i;
+            if (set.contains(i)) continue;
+            sum += i;
         }
 
-        for (int num : numbers) {
-            n += num;
-        }
-
-        return m - n;
+        return sum;
     }
 }
