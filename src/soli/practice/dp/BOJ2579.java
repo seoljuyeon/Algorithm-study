274d5af956cda01e96fb52b0d875015a7950f1a7
luminousol <luminousolding@gmail.com>
2024-03-20 23:55:49 +0900

    ✨feat: BOJ2579.계단 오르기

--- src/soli/practice/dp/BOJ2579.java
+++ src/soli/practice/dp/BOJ2579.java
@@ -11,7 +11,7 @@ public class BOJ2579 {
         int[] num = new int[N+1];
         int[] dp = new int[N+1];
 
-        for(int i = 0 ; i < N ; i++) {
+        for(int i = 1 ; i < N ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             num[i] = Integer.parseInt(st.nextToken());
         }
