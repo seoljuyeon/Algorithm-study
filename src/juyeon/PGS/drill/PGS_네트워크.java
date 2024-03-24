b3b91ab0f5d63aa89faf5eaeefd063e10ae96bb2
seoljuyeon <sxxljxxxxn@gmail.com>
2024-03-24 21:49:34 +0900

    ✨feat: PGS_네트워크

---
+++ src/juyeon/PGS/drill/PGS_네트워크.java
@@ -0,0 +1,35 @@
+package juyeon.PGS.drill;
+
+public class PGS_네트워크 {
+    public static void main(String[] args) {
+        System.out.println(solution(3, new int[][] {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}})); // 2
+    }
+
+    private static boolean[] visited;
+
+    private static int solution(int n, int[][] computers) {
+        int count = 0;
+
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                count++;
+                DFS(i, computers);
+            }
+        }
+
+        return count;
+    }
+
+    private static void DFS(int i, int[][] computers) {
+        visited[i] = true;
+
+        for (int j = 0; j < computers.length; j++) {
+            if (!visited[j] && computers[i][j] == 1) {
+                DFS(j, computers);
+            }
+        }
+
+    }
+}
