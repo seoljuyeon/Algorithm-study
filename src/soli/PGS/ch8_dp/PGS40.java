e7b20c3a517396b8edf9840881883ff795b8312a
luminousol <luminousolding@gmail.com>
2024-04-02 23:12:09 +0900

    ✨feat: PGS40_등굣길

---
+++ src/soli/PGS/ch8_dp/PGS40.java
@@ -0,0 +1,15 @@
+package soli.PGS.ch8_dp;
+
+public class PGS40 {
+
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PGS40 pgs40 = new PGS40();
+
+        System.out.println("Test Case : " + pgs40.solution(4, 3, new int[][]{{2,2}}));
+    }
+}
