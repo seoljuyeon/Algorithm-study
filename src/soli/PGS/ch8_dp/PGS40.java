e167b72ba395d4267f92061d93c60a6663d9bd8f
luminousol <luminousolding@gmail.com>
2024-04-02 23:12:23 +0900

    ✨feat: PGS40_등굣길

--- src/soli/PGS/ch8_dp/PGS40.java
+++ src/soli/PGS/ch8_dp/PGS40.java
@@ -3,8 +3,30 @@ package soli.PGS.ch8_dp;
 public class PGS40 {
 
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        return answer;
+        int mod = 1000000007;
+        int[][] dp = new int[n][m];
+
+        for (int[] puddle : puddles) {
+            dp[puddle[1] - 1][puddle[0] - 1] = -1;
+        }
+
+        dp[0][0] = 1;
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < m ; j++) {
+                if(dp[i][j] == -1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i > 0) {
+                    dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod;
+                }
+                if (j > 0) {
+                    dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod;
+                }
+            }
+        }
+
+        return dp[n-1][m-1] % mod;
     }
 
     public static void main(String[] args) {
