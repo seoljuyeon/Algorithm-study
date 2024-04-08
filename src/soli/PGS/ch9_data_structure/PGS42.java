56f08bc2df38c0e995bd1d52ed41fbf3adb228f1
luminousol <luminousolding@gmail.com>
2024-04-08 21:59:34 +0900

    ✨feat: PGS42_올바른 괄호

--- src/soli/PGS/ch9_data_structure/PGS42.java
+++ src/soli/PGS/ch9_data_structure/PGS42.java
@@ -6,23 +6,19 @@ public class PGS42 {
 
     boolean solution(String s){
 
-        boolean answer = true;
-
         Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()) {
-            if(c == '('){
-                stack.add(c);
+        for(int i = 0 ; i < s.length() ; i++) {
+            if(s.charAt(i) == '('){
+                stack.add(s.charAt(i));
             } else {
                 if(stack.isEmpty()){
-                    return answer = false;
+                    return false;
                 } else {
                     stack.pop();
                 }
             }
         }
-
         return stack.isEmpty();
-
     }
 
     public static void main(String[] args) {
