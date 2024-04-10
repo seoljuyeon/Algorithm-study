6454c810c8150377d6f3b35b7473effda9b9681c
luminousol <luminousolding@gmail.com>
2024-04-11 02:23:25 +0900

    ✨feat:PGS46_다리를 지나는 트럭

--- src/soli/PGS/ch9_data_structure/PGS46.java
+++ src/soli/PGS/ch9_data_structure/PGS46.java
@@ -1,11 +1,45 @@
 package soli.PGS.ch9_data_structure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/42583
 // ✨feat: PGS46_다리를 지나는 트럭
 public class PGS46 {
     public int solution (int bridge_length, int weight, int[] truck_weights){
-        int answer = 0;
-        return answer;
+
+        Queue<Integer> bridge = new LinkedList<>();
+        int time = 0;
+        int now = 0;
+
+        for(int truck : truck_weights) {
+            while (true) {
+                // 다리 비어있으면 트럭 보내기
+                if (bridge.isEmpty()) {
+                    bridge.add(truck);
+                    now += truck;
+                    time++;
+                    break;
+                }
+                // 다리가 다 차면 제일 앞에 빼고 시간 증가
+                else if (bridge.size() == bridge_length) {
+                    weight -= bridge.poll();
+                } else {
+                    if (now + truck <= weight) {
+                        bridge.add(truck);
+                        weight += truck;
+                        time++;
+                        break;
+                    } else {
+                        // 더 못알라가면 다리 비워줌
+                        bridge.add(0);
+                        time++;
+                    }
+                }
+            }
+        }
+
+        return time + bridge_length;
     }
 
     public static void main(String[] args) {
