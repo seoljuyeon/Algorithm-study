ad1bd85588749ebe8eca47c47bd2f97006b24bdc
luminousol <luminousolding@gmail.com>
2024-04-12 17:45:22 +0900

    ✨feat:PGS48_길 찾기 게임

---
+++ src/soli/PGS/ch9_data_structure/PGS48.java
@@ -0,0 +1,15 @@
+package soli.PGS.ch9_data_structure;
+
+public class PGS48 {
+
+    public int[][] solution(int[][] nodeinfo) {
+        int[][] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PGS48 pgs48 = new PGS48();
+
+        System.out.println("TEST CASE :" + pgs48.solution(new int[][] {{5,3}, {11,5}, {13,3},{3,5},{6,1},{1,3},{8,6},{7,2},{2,2}}));
+    }
+}
