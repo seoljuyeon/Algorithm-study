7ea00dc6f2c2f020f63943e4597cf222974b730e
luminousol <luminousolding@gmail.com>
2024-04-15 01:55:59 +0900

    ✨feat:PGS47.순위

--- src/soli/PGS/ch9_data_structure/PGS47.java
+++ src/soli/PGS/ch9_data_structure/PGS47.java
@@ -3,6 +3,13 @@ package soli.PGS.ch9_data_structure;
 import java.util.*;
 
 public class PGS47 {
+
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        return answer;
+    }
+
+
     public static void main(String[] args) {
 
     }
