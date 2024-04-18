04ca2675551ad3ce47e1e5e1660572b0514803fa
luminousol <luminousolding@gmail.com>
2024-04-18 10:02:57 +0900

    ✨feat:PGS50. 디스크 컨트롤러

--- src/soli/PGS/ch9_data_structure/PGS50.java
+++ src/soli/PGS/ch9_data_structure/PGS50.java
@@ -1,57 +1,54 @@
 package soli.PGS.ch9_data_structure;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PGS50 {
-
     public int solution(int[][] jobs) {
-        // 작업 요청 시간에 따라 정렬
-        Arrays.sort(jobs, (a, b) -> (a[0]-b[0]));
 
-        // 소요 시간에 따라 정렬
-        PriorityQueue<int[]> Q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        Arrays.sort(jobs, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+        // Arrays.sort(jobs, (a,b) -> (a[0]-b[0]));
 
-        // 현재 시간
+        PriorityQueue<int[]> Q = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return a[1] - b[1];
+            }
+        });
+        // PriorityQueue<int[]> Q = new PriorityQueue<>((a,b) -> (a[1] - b[1]));
+
         int now = 0;
-        // 대기시간, 소요 시간 합
         int sum = 0;
-        // 배열 순회 인덱스
-        int idx = 0;
-        // 작업 수 추적
         int count = 0;
+        int idx = 0;
 
-        // 작업 전부 끝날 때 까지
         while(count < jobs.length) {
-            // 현재시간 이전에 요청된 작업 큐에 추가
-            // 요청 시간이 현재 시간보다 같거나 작을 때 까지
-            while(idx < jobs.length && jobs[idx][0] <= now  ) {
-                // 작업을 우선 순위 큐에 추가
+            //idx < jobs.length 안하면 IndexError -> 배열 안에서만 접근
+            while(idx < jobs.length && jobs[idx][0] <= now) {
                 Q.add(jobs[idx]);
                 idx++;
             }
-
-            // 우선 순위 큐가 비어있으면 다음 작업 시점으로 시간 이동
-            if(Q.isEmpty()) {
-                now = jobs[idx][0];
-                // 아니면
-            } else {
-                // 우선 순위 큐에서 꺼내서 처리
+            if(!Q.isEmpty()){
                 int[] job = Q.poll();
-                // 현재 시간 업데이트
                 now += job[1];
-                // 대기시간, 처리시간 누적 - 요청시작시간
-                sum += now - job[0] ;
-                // 처리된 작업 수 증가
+                sum += now - job[0];
                 count++;
+            } else {
+                now = jobs[idx][0];
             }
-
         }
-        // 작업 평균 반환
+
         return sum / jobs.length;
     }
 
     public static void main(String[] args) {
-
+        PGS50 pgs50 = new PGS50();
+        System.out.println("TEST CASE " + pgs50.solution(new int[][]{{0,3},{1,9},{2,9}}));
     }
 }
