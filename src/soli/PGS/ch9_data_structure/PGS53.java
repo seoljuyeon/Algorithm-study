6d7263f1ab45a0f63c2fef662877fa97e089e9bf
luminousol <luminousolding@gmail.com>
2024-04-25 10:05:05 +0900

    ✨feat:PGS53. 호텔 방 배정

--- src/soli/PGS/ch9_data_structure/PGS53.java
+++ src/soli/PGS/ch9_data_structure/PGS53.java
@@ -4,19 +4,19 @@ public class PGS53 {
     public long[] solution(long k, long[] room_number) {
 
         // 배정되어 있으면 원하는 방보다 크면서 비어있는 방 중 가장 작은 방
-        long[] answer = new long[room_number];
+        long[] answer = new long[room_number.length];
         boolean[] check = new boolean[(int)k + 1];
 
         for(int i = 0 ; i < room_number.length ; i++) {
-            if(check[room_number[i]] == false) {
-                check[room_number[i]] = true;
+            if(check[(int)room_number[i]] == false) {
+                check[(int)room_number[i]] = true;
                 answer[i] = room_number[i];
             } else {
                 long next = room_number[i];
-                while(check[next]) {
+                while(check[(int)next]) {
                     next++;
                 }
-                check[next] = true;
+                check[(int)next] = true;
                 answer[i] = next;
             }
         }
