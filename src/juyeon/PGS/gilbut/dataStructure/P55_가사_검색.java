bc3fc3716b2193f33da7ab807a5267737a7d8edb
seoljuyeon <sxxljxxxxn@gmail.com>
2024-04-28 23:23:30 +0900

    ✨ feat: P55_가사_검색 ( 효율성 테스트 실패)

---
+++ src/juyeon/PGS/gilbut/dataStructure/P55_가사_검색.java
@@ -0,0 +1,56 @@
+package juyeon.PGS.gilbut.dataStructure;
+
+import java.util.*;
+
+public class P55_가사_검색 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new String[]{"frodo", "front", "frost", "frozen", "frame", "kakao"},
+                new String[]{"fro??", "????o", "fr???", "fro???", "pro?"}))); // 3, 2, 4, 1, 0
+    }
+
+    private static int[] solution(String[] words, String[] queries) {
+
+        // 효율성 테스트 실패
+        int[] answer = new int[queries.length];
+        StringBuilder sb = new StringBuilder();
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            for (int i = 1; i < word.length(); i++) {
+                sb.append(word.substring(0, i));
+
+                while (sb.toString().length() < word.length()) {
+                    sb.append("?");
+                }
+
+                if (sb.toString().length() == word.length()) {
+                    map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+                    sb = new StringBuilder();
+                }
+            }
+
+            sb = new StringBuilder();
+
+            for (int i = 1; i < word.length(); i++) {
+                for (int j = 1; j <= word.length() - i; j++) {
+                    sb.append("?");
+                }
+
+                sb.append(word.substring(word.length() - i, word.length()));
+
+                if (sb.toString().length() == word.length()) {
+                    map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+                    sb = new StringBuilder();
+                }
+            }
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            answer[i] = map.getOrDefault(queries[i], 0);
+        }
+
+        return answer;
+    }
+
+}
