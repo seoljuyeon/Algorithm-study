274791d1425ff837a7a4bf1632287d539dc1f51d
luminousol <luminousolding@gmail.com>
2024-04-30 00:14:33 +0900

    ✨feat:PGS54. 스킬트리

--- src/soli/PGS/ch10_implement/PGS54.java
+++ src/soli/PGS/ch10_implement/PGS54.java
@@ -2,8 +2,29 @@ package soli.PGS.ch10_implement;
 
 public class PGS54 {
     public int solution(String skill, String[] skill_trees) {
-        int answer = -1;
+        int answer = 0;
+
+        char[] order = skill.toCharArray();
+
+
+        for(int i = 0 ; i < skill_trees.length ; i++) {
+            StringBuilder sb = new StringBuilder();
+
+            for(int j = 0 ; j < skill_trees[i].length() ; j++) {
+
+                for(char c : skill.toCharArray()) {
+                    if(skill_trees[i].charAt(j) == c){
+                        sb.append(c);
+                    } else continue;
+                }
+            }
+
+            if(skill.startsWith(sb.toString())) {
+                answer++;
+            }
+        }
         return answer;
+
     }
 
 
