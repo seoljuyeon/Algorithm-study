4bb9c86fa2f4effb2dfca69161b04c0ece855919
luminousol <luminousolding@gmail.com>
2024-04-29 23:41:50 +0900

    ✨feat:PGS54. 스킬트리

---
+++ src/soli/PGS/ch10_implement/PGS54.java
@@ -0,0 +1,16 @@
+package soli.PGS.ch10_implement;
+
+public class PGS54 {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = -1;
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        PGS54 pgs54 = new PGS54();
+
+        System.out.println(pgs54.solution("CBD", new String[] {"BACDE", "CBADF", "AECB", "BDA"}));
+    }
+}
