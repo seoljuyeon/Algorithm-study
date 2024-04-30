868612d49411708c015ab1931ee208f7d6a9e054
luminousol <luminousolding@gmail.com>
2024-04-30 22:38:51 +0900

    ✨feat:PGS55. 키패드 누르기

--- src/soli/PGS/ch10_implement/PGS55.java
+++ src/soli/PGS/ch10_implement/PGS55.java
@@ -2,8 +2,17 @@ package soli.PGS.ch10_implement;
 
 public class PGS55 {
 
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        return answer;
+    }
 
     public static void main(String[] args) {
 
+        PGS55 pgs55 = new PGS55();
+
+        System.out.println(pgs55.solution( new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5} , "right"));
+        System.out.println(pgs55.solution( new int[]{7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2} , "left"));
+        System.out.println(pgs55.solution( new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0} , "right"));
     }
 }
