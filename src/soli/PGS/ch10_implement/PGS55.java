e4dea66cb7ac68d58fb6add96515f60be98746cd
luminousol <luminousolding@gmail.com>
2024-05-01 13:25:09 +0900

    ✨feat:PGS55. 키패드 누르기

--- src/soli/PGS/ch10_implement/PGS55.java
+++ src/soli/PGS/ch10_implement/PGS55.java
@@ -3,8 +3,56 @@ package soli.PGS.ch10_implement;
 public class PGS55 {
 
     public String solution(int[] numbers, String hand) {
-        String answer = "";
-        return answer;
+
+        int[][] keypad = {{3,1},
+                {0,0}, {0,1}, {0,2},
+                {1,0}, {1,1}, {1,2},
+                {2,0}, {2,1}, {2,2},
+                {3,0}, {3,1}, {3,2}};
+
+        int[] lefthand = keypad[10];
+        int[] righthand = keypad[12];
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int num : numbers) {
+
+            int[] enter = keypad[num];
+
+            if((num == 1) || (num == 4) || (num==7)) {
+                sb.append("L");
+                lefthand = keypad[num];
+            }
+
+            else if((num == 3) || (num == 6) || (num == 9)) {
+                sb.append("R");
+                righthand = keypad[num];
+            }
+
+            else {
+                int left = Math.abs(lefthand[0] - enter[0]) + Math.abs(lefthand[1] - enter[1]);
+                int right = Math.abs(righthand[1] - enter[1]) + Math.abs(righthand[1] - enter[1]);
+
+                if(left < right) {
+                    sb.append("L");
+                    lefthand = keypad[num];
+                } else if(left > right) {
+                    sb.append("R");
+                    righthand = keypad[num];
+                } else {
+                    if(hand.equals("right")) {
+                        sb.append("R");
+                        righthand = keypad[num];
+                    }
+                    else {
+                        sb.append("L");
+                        lefthand = keypad[num];
+                    }
+                }
+            }
+        }
+        return sb.toString();
+
     }
 
     public static void main(String[] args) {
