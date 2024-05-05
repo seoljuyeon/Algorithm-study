f4aa2ddc0d696747debd436ed95a10b6e3a2ebd3
luminousol <luminousolding@gmail.com>
2024-05-05 11:26:20 +0900

    ✨feat:PGS57. 네트워크

--- src/soli/PGS/ch10_implement/PGS57.java
+++ src/soli/PGS/ch10_implement/PGS57.java
@@ -1,4 +1,34 @@
 package soli.PGS.ch10_implement;
 
 public class PGS57 {
+
+    public int solution(int n, int[][] computers){
+
+        boolean[] visited = new boolean[n];
+
+        int answer = 0;
+
+        for(int i = 0; i < n ; i++) {
+            if(!visited[i]){
+                dfs(n, computers, i, visited);
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public void dfs(int n, int[][] computers, int node , boolean[] visited) {
+        visited[node] = true;
+
+        for(int i = 0 ; i < n ; i++) {
+            if(computers[node][i] == 1 && !visited[i]) {
+                dfs(n, computers, node, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
