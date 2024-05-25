b4daa05a8b641b9ebdbfae48d187af5c5aff23a3
luminousol <luminousolding@gmail.com>
2024-05-25 23:30:03 +0900

    ✨feat: PGS68. 양궁대회

---
+++ src/soli/PGS/ch11_kakao/PGS68.java
@@ -0,0 +1,12 @@
+package soli.PGS.ch11_kakao;
+
+public class PGS68 {
+    public int[] solution(int n, int[] info) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
