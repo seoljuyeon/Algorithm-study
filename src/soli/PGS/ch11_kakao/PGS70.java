af4997eab50e05d6814c0aa95420641351ed35d6
luminousol <luminousolding@gmail.com>
2024-05-27 10:31:58 +0900

    ✨feat: PGS70. 파괴되지 않은 건물

---
+++ src/soli/PGS/ch11_kakao/PGS70.java
@@ -0,0 +1,14 @@
+package soli.PGS.ch11_kakao;
+
+public class PGS70 {
+
+    public int solution(int[][] board, int[][] skill){
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
