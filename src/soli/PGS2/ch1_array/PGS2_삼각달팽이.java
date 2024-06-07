478f3a3884a93d180812ce6bf24732ae32300e23
luminousol <luminousolding@gmail.com>
2024-06-07 22:48:11 +0900

    feat✨ : PGS2. 삼각달팽이

---
+++ src/soli/PGS2/ch1_array/PGS2_삼각달팽이.java
@@ -0,0 +1,13 @@
+package soli.PGS2.ch1_array;
+
+public class PGS2_삼각달팽이 {
+    public int[] solution(int n){
+        int[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
