dc9589123df767c76ba18d63b3e418b28c4edd0c
luminousol <luminousolding@gmail.com>
2024-06-14 15:44:31 +0900

    feat✨ : PGS7.이상한 문자 만들기

--- src/soli/PGS2/ch2_str/PGS7_이상한문자만들기.java
+++ src/soli/PGS2/ch2_str/PGS7_이상한문자만들기.java
@@ -1,7 +1,34 @@
 package soli.PGS2.ch2_str;
 
-public class PGS7_이상한문자만들기 {
-    public static void main(String[] args) {
+import soli.PGS.ch2_string.PGS7;
 
+public class PGS7_이상한문자만들기 {
+    public String solution(String s) {
+
+        String[] words = s.split(" ", -1);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0 ; i < words.length ; i++){
+            for(int j = 0 ; j < words[i].length() ; j++){
+                if(j%2==0){
+                    sb.append(Character.toUpperCase(words[i].charAt(j)));
+                } else {
+                    sb.append(Character.toLowerCase(words[i].charAt(j)));
+                }
+            }
+            if(i < words.length -1) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        PGS7 pgs7 = new PGS7();
+
+        System.out.println("Test Case1 : " + pgs7.solution("try hello world"));
+        System.out.println("Test Case2 : " + pgs7.solution("space  bar  here  "));
     }
 }
