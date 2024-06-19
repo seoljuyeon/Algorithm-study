21b4568e1c2c33282c61d0401b368635010de25a
luminousol <luminousolding@gmail.com>
2024-06-19 22:28:39 +0900

    feat✨ : PGS13.문자열다루기기본

--- src/soli/PGS2/ch2_str/PGS13_문자열다루기기본.java
+++ src/soli/PGS2/ch2_str/PGS13_문자열다루기기본.java
@@ -1,7 +1,23 @@
 package soli.PGS2.ch2_str;
 
-public class PGS13_문자열다루기기본 {
-    public static void main(String[] args) {
+import soli.PGS.ch2_string.PGS13;
 
+public class PGS13_문자열다루기기본 {
+
+    public boolean solution(String s) {
+
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        if (s.matches("[0-9]+")){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PGS13_문자열다루기기본 pgs13 = new PGS13_문자열다루기기본();
+        System.out.println("Test Case1 : " + pgs13.solution("a234"));
+        System.out.println("Test Case1 : " + pgs13.solution("1234"));
     }
 }
