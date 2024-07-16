3eb0a74210bd95419db26d5aa775da41c26c8c72
luminousol <luminousolding@gmail.com>
2024-07-16 09:25:05 +0900

    feat✨ : ELC 6. 빨간 선과 파란선

--- src/soli/ELC/ELC6_빨간선과파란선.java
+++ src/soli/ELC/ELC6_빨간선과파란선.java
@@ -6,38 +6,35 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class ELC6_빨간선과파란선 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        int[] a = new int[N-1];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N-1 ; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] a = new int[N - 1];
+        for (int i = 0; i < N - 1; i++) {
+            a[i] = scanner.nextInt();
         }
-
         Map<List<Integer>, Integer> dp = new HashMap<>();
         dp.put(new ArrayList<>(Collections.nCopies(N, 1)), 0);
-        Deque<List<Integer>> q = new ArrayDeque<>();
-        q.add(new ArrayList<>(Collections.nCopies(N, 1)));
-        while (!q.isEmpty()) {
-            List<Integer> v = q.pollFirst();
-            for(int i = 0 ; i < v.size() ; i++) {
-                for (int j = i +1 ; j < v.size() ; j++) {
+        Deque<List<Integer>> queue = new ArrayDeque<>();
+        queue.add(new ArrayList<>(Collections.nCopies(N, 1)));
+        while (!queue.isEmpty()) {
+            List<Integer> v = queue.pollFirst();
+            for (int i = 0; i < v.size(); i++) {
+                for (int j = i + 1; j < v.size(); j++) {
                     List<Integer> u = new ArrayList<>();
-                    for(int k = 0 ; k < v.size() ; k++) {
-                        if(k ==i) {
+                    for (int k = 0; k < v.size(); k++) {
+                        if (k == i) {
                             u.add(v.get(i) + v.get(j));
-                        } else if(k != j) {
+                        } else if (k != j) {
                             u.add(v.get(k));
                         }
-                    } Collections.sort(u);
-                    if(!dp.containsKey(u)) {
-                        q.add(u);
-                        dp.put(u, dp.get(v) + (1 - a[N-v.size()]* v.get(i) * v.get(j)));
+                    }
+                    Collections.sort(u);
+                    if (!dp.containsKey(u)) {
+                        queue.add(u);
+                        dp.put(u, dp.get(v) + (1 - a[N - v.size()]) * v.get(i) * v.get(j));
                     } else {
-                        dp.put(u, Math.min(dp.get(u), dp.get(v) + (1 - a[N-v.size()] * v.get(i) * v.get(j))));
+                        dp.put(u, Math.min(dp.get(u), dp.get(v) + (1 - a[N - v.size()]) * v.get(i) * v.get(j)));
                     }
                 }
             }
