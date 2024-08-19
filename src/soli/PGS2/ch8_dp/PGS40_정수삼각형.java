db991328149dbc435cb039d583f67507807cbfd7
luminousol <luminousolding@gmail.com>
2024-08-19 20:56:56 +0900

    feat✨ : PGS40.정수삼각형

--- src/soli/PGS2/ch8_dp/PGS40_정수삼각형.java
+++ src/soli/PGS2/ch8_dp/PGS40_정수삼각형.java
@@ -2,25 +2,23 @@ package soli.PGS2.ch8_dp;
 
 public class PGS40_정수삼각형 {
 
-
-
     public static int solution(int[][] triangle) {
         int[][] tri = new int[triangle.length][triangle.length];
         tri[0][0] = triangle[0][0];
 
-        for (int i = 1; i < tri.length; i++) {
-            tri[i][0] = tri[i - 1][0] + triangle[0][0];
+        for(int i = 1 ; i < tri.length ; i++) {
+            tri[i][0] =  tri[i-1][0] + triangle[i][0];
 
-            for (int j = 1; j <= i; j++) {
-                tri[i][j] = Math.max(tri[i - 1][j], tri[i - 1][j - 1]) + triangle[i][j];
+            for(int j = 1 ; j <= i ; j++) {
+                tri[i][j] = Math.max(tri[i-1][j], tri[i-1][j-1]) + triangle[i][j];
             }
-            tri[i][j] = tri[i - 1][j - 1] + triangle[i][j];
+            tri[i][i] = tri[i-1][i-1] + triangle[i][i];
         }
 
         int answer = 0;
 
-        for (int i = 0; i < triangle.length; i++) {
-            answer = Math.max(answer, dp[triangle.length - 1][i]);
+        for(int i = 0 ; i <  triangle.length ; i++) {
+            answer = Math.max(answer, tri[triangle.length - 1][i]);
         }
 
         return answer;
