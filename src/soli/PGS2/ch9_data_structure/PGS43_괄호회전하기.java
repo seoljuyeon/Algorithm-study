6820cc80408b3e83f05a61b350a0a0ee254b18de
luminousol <luminousolding@gmail.com>
2024-08-25 16:52:15 +0900

    feat✨ : PGS43.괄호회전하기

--- src/soli/PGS2/ch9_data_structure/PGS43_괄호회전하기.java
+++ src/soli/PGS2/ch9_data_structure/PGS43_괄호회전하기.java
@@ -1,13 +1,39 @@
 package soli.PGS2.ch9_data_structure;
 
-import soli.PGS.ch9_data_structure.PGS43;
+import java.util.Stack;
 
 public class PGS43_괄호회전하기 {
     public int solution(String s) {
 
         int answer = 0;
 
+        for(int i = 0 ; i < s.length() ; i++) {
+            if(isCorrect(s.substring(i)+ s.substring(0, i))){
+                answer++;
+            }
+        }
+
         return answer;
+    }
+
+    private boolean isCorrect(String newString) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0 ; i < newString.length() ; i++) {
+
+            char c = newString.charAt(i);
+
+            if(c == '(' || c == '[' || c == '{') {
+                stack.add(c);
+            } else {
+                if(stack.isEmpty()) return false;
+
+                char pop = stack.pop();
+                if((c==')' && pop != '(')|| (c==']' && pop != '[') && (c=='}' && c != '{')) return false;
+            }
+        }
+        return stack.isEmpty(); // 결과 유효한지 확인
     }
 
     public static void main(String[] args) {
