88e0121b51b5c9c45623cfdfb6a3202ae990db98
luminousol <luminousolding@gmail.com>
2024-09-05 21:13:34 +0900

    :sparkles: feat : PGS50_이중우선순위큐

--- src/soli/PGS2/ch9_data_structure/PGS50_이중우선순위큐.java
+++ src/soli/PGS2/ch9_data_structure/PGS50_이중우선순위큐.java
@@ -1,50 +1,42 @@
 package soli.PGS2.ch9_data_structure;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PGS50_이중우선순위큐 {
 
     public int[] solution(String[] operations) {
 
-        int[] answer = new int[2];
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> min = new PriorityQueue<>();
 
-        PriorityQueue<Integer> minQ = new PriorityQueue<>();
-        PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
-        int queueSize = 0;
+        int size = 0;
 
-        for(String s : operations) {
-            if(s.startsWith("I ")) {
-                int num = Integer.parseInt(s.substring(2));
-                minQ.add(num);
-                maxQ.add(num);
-                queueSize++;
-            } else if(s.startsWith("D -")) {
-                if (!minQ.isEmpty()) {
-                    int min = minQ.poll();
-                    maxQ.remove(min);
-                    queueSize--;
+        for(String operation : operations) {
+            if(operation.startsWith("I ")) {
+
+                int num = Integer.parseInt(operation.substring(2));
+
+                max.add(num);
+                min.add(num);
+                size++;
+
+            } else if(operation.equals("D -1")){
+                if(size > 0) {
+                    int minnum = min.poll();
+                    max.remove(minnum);
+                    size--;
                 }
-            } else if(s.startsWith("D 1")) {
-                if(!maxQ.isEmpty()){
-                    int max = maxQ.poll();
-                    minQ.remove(max);
-                    queueSize--;
+            } else {
+                if(!max.isEmpty()) {
+                    int maxnum = max.poll();
+                    min.remove(maxnum);
+                    size--;
                 }
             }
         }
 
-        if(queueSize > 0) {
-            answer[0] = maxQ.peek();
-            answer[1] = minQ.peek();
-        }
+        return size>0 ? new int[]{max.peek(), min.peek()} : new int[]{0, 0};
 
-
-        return answer;
     }
-
-
-
 }
